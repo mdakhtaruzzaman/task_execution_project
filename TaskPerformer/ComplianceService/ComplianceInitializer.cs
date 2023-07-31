4438ee1c32383f80169dc356fb045c297cc05cdf
Akhtar Uzzaman <md.akhtar.uzzaman@bd.ey.com>
2023-07-31 17:46:46 +0600

    some issue fixed

---
+++ TaskPerformer/ComplianceService/ComplianceInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskPerformer.Model;
+
+namespace TaskPerformer.ComplianceService
+{
+    internal class ComplianceInitializer
+    {
+        public void Init(Invoker invoker, Compliance compliance, CompanyData companyData)
+        {
+            switch (compliance.Id)
+            {
+                case 1:
+                    invoker.SetCommand(new CompanySizeCompliance(companyData));
+                    break;
+                case 2:
+                    invoker.SetCommand(new Female30PercentCompliance(companyData));
+                    break;
+                case 3:
+                    invoker.SetCommand(new Male50PercentCompliance(companyData));
+                    break;
+                case 4:
+                    invoker.SetCommand(new Female50PercentCompliance(companyData));
+                    break;
+                case 5:
+                    invoker.SetCommand(new Age30PercemtAbove50Compliance(companyData));
+                    break;
+                default:
+                    break;
+            }
+
+            // Invoker executes the command
+            invoker.ExecuteTask();
+        }
+    }
+}
--- TaskPerformer/Program.cs
+++ TaskPerformer/Program.cs
@@ -26,47 +26,19 @@ string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "res", "MO
 if (File.Exists(filePath))
 {
     var data = fileReader.Read(filePath);
-    foreach (var single_data in data)
+    foreach (var companyData in data)
     {
         Console.WriteLine("############################### Start ###########################");
-        Console.WriteLine("Company Name: " + single_data.CompanyName);
-        Console.WriteLine($"State: {single_data.State}\tTotal Emp: {single_data.NumberOfEmp}\tMale Emp: {single_data.MaleEmp}\tFemale Emp: {single_data.FemaleEmp}\tAbove 50 Age: {single_data.Above50Age}");
+        Console.WriteLine($"ID: {companyData.Id}\tCompany Name: {companyData.CompanyName}\tState: {companyData.State}\tTotal Emp: {companyData.NumberOfEmp}\tMale Emp: {companyData.MaleEmp}\tFemale Emp: {companyData.FemaleEmp}\tAbove 50 Age: {companyData.Above50Age}");
         Console.WriteLine();
-        // Client creates commands and associates them with the receiver
-        IComplianceExecutor companySizeCompliance = new CompanySizeCompliance(single_data);
-        IComplianceExecutor female30PercentCompliance = new Female30PercentCompliance(single_data);
-        IComplianceExecutor male50PercentCompliance = new Male50PercentCompliance(single_data);
-        IComplianceExecutor female50PercentCompliance = new Female50PercentCompliance(single_data);
-        IComplianceExecutor age30PercemtAbove50Compliance = new Age30PercemtAbove50Compliance(single_data);
 
         // Client creates the invoker and associates it with the commands
         Invoker invoker = new Invoker();
-        var stateCompliance = stateCompliances.Where(_ => _.State.Equals(single_data.State)).FirstOrDefault();
+        StateCompliance stateCompliance = stateCompliances.Where(_ => _.State.Equals(companyData.State)).FirstOrDefault();
         foreach (var item in stateCompliance.Compliances)
         {
-            switch (item.Id)
-            {
-                case 1:
-                    invoker.SetCommand(companySizeCompliance);
-                    break;
-                case 2:
-                    invoker.SetCommand(female30PercentCompliance);
-                    break;
-                case 3:
-                    invoker.SetCommand(male50PercentCompliance);
-                    break;
-                case 4:
-                    invoker.SetCommand(female50PercentCompliance);
-                    break;
-                case 5:
-                    invoker.SetCommand(age30PercemtAbove50Compliance);
-                    break;
-                default:
-                    break;
-            }
-
-            // Invoker executes the command
-            invoker.ExecuteTask();
+            ComplianceInitializer complianceInitializer = new ComplianceInitializer();
+            complianceInitializer.Init(invoker, item, companyData);
         }
 
         Console.WriteLine();
